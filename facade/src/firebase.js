c77dd6b7df6851eccf2833ebbb1c3b862bcf0c0d
Dadon-Ravin <danieldonaldravin@gmail.com>
2025-08-06 09:51:20 -0500

    fixed firebase api key

--- facade/src/firebase.js
+++ facade/src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBnbaJKpY3K8H2E3879ht3hdpxGu8ClLH8",
-    authDomain: "facade-game.firebaseapp.com",
-    projectId: "facade-game",
-    storageBucket: "facade-game.firebasestorage.app",
-    messagingSenderId: "677958392042",
-    appId: "1:677958392042:web:542efdc81dbbff1fa69f93",
-    measurementId: "G-6MZE8XY7DB"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
